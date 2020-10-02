aa5a3e9aef3092d4df6295a62bf8930a60281893
Protik Hosen <protik3380@gmail.com>
2020-10-02 16:30:20 +0600

    api call

--- screens/Homescreen.js
+++ screens/Homescreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
   Text,
@@ -7,8 +7,27 @@ import {
   TouchableOpacity,
   Button,
 } from "react-native";
+import { headerConfig } from "../utils/config";
+import axios from "axios";
 
 const HomeScreen = ({ navigation }) => {
+  const Base_url = "http://139.162.14.41:5000/"; // stage server
+  const [allUser, setAllUser] = useState();
+  const url = Base_url + `getallRfidUsersByDeviceLocation`;
+
+  useEffect(() => {
+    console.log("use effect");
+    const getData = async () => {
+      await headerConfig().then((result) => {
+        axios.get(url, result).then((data) => {
+          setAllUser(data.data.count);
+          console.log("all user ", allUser);
+        });
+      });
+    };
+    getData();
+  }, [allUser]);
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
